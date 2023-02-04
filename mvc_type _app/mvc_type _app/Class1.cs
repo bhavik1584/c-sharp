d0d75b971b6cc5224343a4f51dd8aad5ad28d849
official_gamer <102470521+bhavik1584@users.noreply.github.com>
2023-02-04 19:00:52 +0530

    Add files via upload
    update function not working

---
+++ mvc_type _app/mvc_type _app/Class1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using System.Windows.Forms;
+
+namespace mvc_type__app
+{
+    internal class Class1
+    {
+
+        public static SqlConnection sc = new SqlConnection("Data Source=(LocalDB)\\MSSQLLocalDB;AttachDbFilename=C:\\Users\\bhavi\\Documents\\VIP.mdf;Integrated Security=True;Connect Timeout=30");
+        
+        public static string arraystr;
+        public static string str;
+
+
+        //done
+
+        public static string insert(string tname,string[]args)
+        {
+
+            //normal version
+
+            /* string str = "INSERT INTO '" + tname + "' VALUES(";
+
+             for(int i=0;i<args.Length;i++)
+             {
+                 str += args[i]+",";
+             }
+
+             str +=")";
+
+             return str;
+
+             */
+           
+
+          //refiendment version
+
+             arraystr = string.Join(",", args);
+
+             str = $"INSERT INTO {tname} VALUES();";
+
+             str=str.Insert(str.IndexOf('(')+1, arraystr);
+
+
+                SqlDataAdapter da = new SqlDataAdapter(str, sc);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+
+            MessageBox.Show("data insert sucessfully");
+
+            return str;
+
+        }
+
+
+        //done
+
+        public static string selectAllData(string tname)
+        {
+            str = $"SELECT * FROM {tname}";
+
+            SqlDataAdapter da = new SqlDataAdapter(str, sc);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            return str;
+        }
+
+
+        //done
+        public static string selectParticulerData(string tname,int id)
+        {
+            str = $"SELECT * FROM {tname} WHERE id={id}";
+
+            SqlDataAdapter da = new SqlDataAdapter(str, sc);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+            return str;
+        }
+
+
+        //some issue
+        public static string update(string tname, int id ,string[] Fargs,string[] Uargs)
+        {
+
+             str = $"UPDATE {tname} SET  WHERE id = {id}";
+             
+            
+
+             arraystr = string.Join($"={Uargs},", Fargs);
+
+             str=str.Insert(str.LastIndexOf('T')+2,arraystr);
+
+            
+
+            return str;
+        }
+
+       
+       // done
+        public static string delete(string tname,int id)
+        {
+            str = $"DELETE FROM {tname} WHERE id = {id}";
+
+            SqlDataAdapter da = new SqlDataAdapter(str, sc);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+
+
+            return str;
+
+        }
+
+    }
+}
---
+++ mvc_type _app/mvc_type _app/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace mvc_type__app
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+
+            string[] data = {$"'{textBox1.Text}'",$"'{textBox2.Text}'",$"'{textBox3.Text}'" };
+
+            Class1.insert("stud",data);
+            
+
+            
+            
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string tname = "hello";
+            string[] Fargs = { "h1", "h2", "h3", "h4", "h5", "h6" };
+            string[] Uargs = { "A", "B", "C", "D", "E", "F" };
+            string result = Class1.update(tname, 1, Fargs, Uargs);
+
+
+            MessageBox.Show(result);
+        }
+    }
+}
